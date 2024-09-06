f819565e0003ba379860d3082423dbcc4487e74a
choi-ys <project.log.062@gmail.com>
2024-09-07 04:08:08 +0900

    test: presentation 계층의 일관된 TC 작성을 위한 환경 설정 구성

---
+++ web-common/src/test/java/io/fulflix/common/base/TestBase.java
@@ -0,0 +1,10 @@
+package io.fulflix.common.base;
+
+import static org.springframework.test.context.TestConstructor.AutowireMode.ALL;
+
+import org.springframework.test.context.TestConstructor;
+
+@TestConstructor(autowireMode = ALL)
+public abstract class TestBase {
+
+}
---
+++ web-common/src/test/java/io/fulflix/common/base/presentation/WebMvcTestBase.java
@@ -0,0 +1,18 @@
+package io.fulflix.common.base.presentation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.fulflix.common.base.TestBase;
+import jakarta.annotation.Resource;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+public abstract class WebMvcTestBase extends TestBase {
+
+    @Resource
+    protected MockMvc mockMvc;
+
+    @Resource
+    protected ObjectMapper objectMapper;
+
+}
