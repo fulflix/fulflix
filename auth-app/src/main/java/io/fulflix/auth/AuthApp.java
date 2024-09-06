d0b3027a2b34bb59f2391f11a6d68c1638e3898d
choi-ys <project.log.062@gmail.com>
2024-09-07 00:41:27 +0900

    build: initial auth-app module for authentication and authorization
    add dependencies for auth-app
    - web-common module
    - spring-security
    - h2
    
    add web-common configurations to auth-app entry point
    - set base-packages path
    - set common application.yml and auth-app application.yml
    
    register auth-app to settings.gradle

---
+++ auth-app/src/main/java/io/fulflix/auth/AuthApp.java
@@ -0,0 +1,23 @@
+package io.fulflix.auth;
+
+import static io.fulflix.common.web.utils.PropertiesCombineUtils.BASE_PACKAGE;
+import static io.fulflix.common.web.utils.PropertiesCombineUtils.CONFIGURATION_PROPERTIES_NAME;
+import static io.fulflix.common.web.utils.PropertiesCombineUtils.combinedApplicationProperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = BASE_PACKAGE)
+public class AuthApp {
+
+    private static final String AUTH_APP_PROPERTY = "application-auth";
+
+    public static void main(String[] args) {
+        System.setProperty(
+            CONFIGURATION_PROPERTIES_NAME,
+            combinedApplicationProperties(AUTH_APP_PROPERTY)
+        );
+        SpringApplication.run(AuthApp.class, args);
+    }
+
+}
