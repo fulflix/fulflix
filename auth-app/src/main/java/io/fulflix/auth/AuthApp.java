aa212f34f992f2ad6c83a5fecbe68aa2f854021e
choi-ys <project.log.062@gmail.com>
2024-09-07 20:46:00 +0900

    config: jwt 발급 항목 주입을 위한 환경 설정 구성

--- auth-app/src/main/java/io/fulflix/auth/AuthApp.java
+++ auth-app/src/main/java/io/fulflix/auth/AuthApp.java
@@ -4,10 +4,13 @@ import static io.fulflix.common.web.utils.PropertiesCombineUtils.BASE_PACKAGE;
 import static io.fulflix.common.web.utils.PropertiesCombineUtils.CONFIGURATION_PROPERTIES_NAME;
 import static io.fulflix.common.web.utils.PropertiesCombineUtils.combinedApplicationProperties;
 
+import io.fulflix.auth.utils.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(scanBasePackages = BASE_PACKAGE)
+@EnableConfigurationProperties(JwtProperties.class)
 public class AuthApp {
 
     private static final String AUTH_APP_PROPERTY = "application-auth";
