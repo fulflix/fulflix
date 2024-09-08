8c16a6145ac9f91221c6ad7d80dd22994ea4c2a5
choi-ys <project.log.062@gmail.com>
2024-09-08 14:01:29 +0900

    config: auth-app logging 설정 추가

--- auth-app/src/main/java/io/fulflix/auth/AuthApp.java
+++ auth-app/src/main/java/io/fulflix/auth/AuthApp.java
@@ -1,8 +1,6 @@
 package io.fulflix.auth;
 
 import static io.fulflix.common.web.utils.PropertiesCombineUtils.BASE_PACKAGE;
-import static io.fulflix.common.web.utils.PropertiesCombineUtils.CONFIGURATION_PROPERTIES_NAME;
-import static io.fulflix.common.web.utils.PropertiesCombineUtils.combinedApplicationProperties;
 
 import io.fulflix.auth.utils.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
@@ -13,13 +11,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties(JwtProperties.class)
 public class AuthApp {
 
-    private static final String AUTH_APP_PROPERTY = "application-auth";
-
     public static void main(String[] args) {
-        System.setProperty(
-            CONFIGURATION_PROPERTIES_NAME,
-            combinedApplicationProperties(AUTH_APP_PROPERTY)
-        );
         SpringApplication.run(AuthApp.class, args);
     }
 
