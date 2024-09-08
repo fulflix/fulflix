8ee651fe729b26a648d783aee20b3ba2edbf6b1c
choi-ys <project.log.062@gmail.com>
2024-09-09 03:54:10 +0900

    refactor: 201 Created 응답 처리 수정

--- auth-app/src/main/java/io/fulflix/auth/api/AuthController.java
+++ auth-app/src/main/java/io/fulflix/auth/api/AuthController.java
@@ -1,6 +1,7 @@
 package io.fulflix.auth.api;
 
 import static io.fulflix.auth.api.AuthController.BASE_AUTH_PATH;
+import static io.fulflix.common.web.utils.ResponseEntityUtils.created;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
@@ -9,14 +10,12 @@ import io.fulflix.auth.api.dto.SignUpRequest;
 import io.fulflix.auth.application.AuthenticationService;
 import io.fulflix.auth.application.AuthorizationService;
 import jakarta.servlet.http.HttpServletResponse;
-import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping(
@@ -35,12 +34,10 @@ public class AuthController {
 
     @PostMapping("/sign-up")
     ResponseEntity<Void> signUp(@RequestBody SignUpRequest signupRequest) {
-        URI userDetailsUri = UriComponentsBuilder.fromUriString(USER_DETAILS_URI_FORMAT)
-            .buildAndExpand(authorizationService.authorization(signupRequest))
-            .toUri();
-
-        return ResponseEntity.created(userDetailsUri)
-            .build();
+        return created(
+            USER_DETAILS_URI_FORMAT,
+            authorizationService.authorization(signupRequest)
+        );
     }
 
     @PostMapping("/sign-in")
