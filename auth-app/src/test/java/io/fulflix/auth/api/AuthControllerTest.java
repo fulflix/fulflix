8c543004e3dee8d3a108a88420fedb65f62ad18c
choi-ys <project.log.062@gmail.com>
2024-09-09 03:55:02 +0900

    test: 인증 API 예외 TC 추가 및 event listener 동작 여부 확인을 위한 설정 및 검증 부 추가

--- auth-app/src/test/java/io/fulflix/auth/api/AuthControllerTest.java
+++ auth-app/src/test/java/io/fulflix/auth/api/AuthControllerTest.java
@@ -6,24 +6,26 @@ import static io.fulflix.auth.fixture.AuthTestFixture.HUB_COMPANY;
 import static io.fulflix.auth.fixture.AuthTestFixture.HUB_DELIVERY_MANAGER;
 import static io.fulflix.auth.fixture.AuthTestFixture.MASTER_ADMIN;
 import static io.fulflix.auth.fixture.AuthTestFixture.SUPPLY_COMPANY;
-import static io.fulflix.auth.fixture.AuthTestFixture.fixtureGenerator;
-import static io.fulflix.auth.utils.jwt.TokenTestHelper.TEST_JWT_PROPERTIES;
-import static io.fulflix.auth.utils.jwt.TokenTestHelper.TEST_PRINCIPAL;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import io.fulflix.auth.api.dto.SignInRequest;
 import io.fulflix.auth.api.dto.SignUpRequest;
 import io.fulflix.auth.domain.Role;
-import io.fulflix.auth.utils.jwt.JwtProvider;
+import io.fulflix.auth.exception.AuthErrorCode;
+import io.fulflix.auth.exception.AuthException;
+import io.fulflix.common.web.exception.event.ThrowsExceptionEvent;
+import io.fulflix.common.web.exception.event.ThrowsExceptionEventListener;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.MimeTypeUtils;
 
@@ -32,6 +34,9 @@ class AuthControllerTest extends AuthApiTestHelper {
 
     private static final String SIGN_UP_URL = "/auth/sign-up";
     private static final String SIGN_IN_URL = "/auth/sign-in";
+
+    @SpyBean
+    protected ThrowsExceptionEventListener throwsExceptionEventListener;
 
     @ParameterizedTest(name = "[{index}][{1}]")
     @MethodSource
@@ -67,8 +72,6 @@ class AuthControllerTest extends AuthApiTestHelper {
     @DisplayName("[로그인][POST:200]")
     void signIn() throws Exception {
         // Given
-        SignInRequest signInRequest = fixtureGenerator.giveMeOne(SignInRequest.class);
-        String accessToken = JwtProvider.create(TEST_JWT_PROPERTIES, TEST_PRINCIPAL);
         given(authenticationService.authenticate(signInRequest)).willReturn(accessToken);
 
         // When
@@ -81,6 +84,30 @@ class AuthControllerTest extends AuthApiTestHelper {
         // Then
         resultActions.andDo(print())
             .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("[로그인][POST:403]")
+    void throwAuthException() throws Exception {
+        // Given
+        given(authenticationService.authenticate(signInRequest))
+            .willThrow(new AuthException(AuthErrorCode.BAD_CREDENTIALS));
+
+        // When
+        ResultActions resultActions = mockMvc.perform(post(SIGN_IN_URL)
+            .contentType(MimeTypeUtils.APPLICATION_JSON_VALUE)
+            .accept(MimeTypeUtils.APPLICATION_JSON_VALUE)
+            .content(objectMapper.writeValueAsBytes(signInRequest))
+        );
+
+        // Then
+        resultActions.andDo(print())
+            .andExpect(status().isUnauthorized());
+
+        ArgumentCaptor<ThrowsExceptionEvent> eventCaptor =
+            ArgumentCaptor.forClass(ThrowsExceptionEvent.class);
+        verify(throwsExceptionEventListener)
+            .onThrowsExceptionLogging(eventCaptor.capture());
     }
 
 }
