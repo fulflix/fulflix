94db821ddc4445d7a2b9738da281c915e8d8da5f
choi-ys <project.log.062@gmail.com>
2024-09-09 05:36:05 +0900

    typo: 오류 메세지 맞춤법 수정

--- web-common/src/main/java/io/fulflix/common/web/exception/response/GlobalErrorCode.java
+++ web-common/src/main/java/io/fulflix/common/web/exception/response/GlobalErrorCode.java
@@ -1,4 +1,4 @@
-package io.fulflix.common.web.exception;
+package io.fulflix.common.web.exception.response;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
@@ -13,11 +13,11 @@ public enum GlobalErrorCode {
     MISSING_PATH_VARIABLE(BAD_REQUEST, "잘못된 요청입니다. Path URI를 확인해 주세요."),
     MISSING_SERVLET_REQUEST_PARAMETER(BAD_REQUEST, "잘못된 요청입니다. Query Parameter를 확인해 주세요."),
     METHOD_ARGUMENT_TYPE_MISMATCH(BAD_REQUEST, "요청 값의 자료형이 잘못되었습니다."),
-    METHOD_ARGUMENT_NOT_VALID(BAD_REQUEST, "잘못된 요청입니다. Request Body를 확인해 주세요."),
-    HTTP_MESSAGE_NOT_READABLE(BAD_REQUEST, "요청 필드의 값이 잘못 되었습니다."),
-    HTTP_MEDIA_TYPE_NOT_ACCEPTABLE(BAD_REQUEST, "응답 형식을 처리할 수 없습니다. 잠시 후 다시 시도해주세요."),
-    HTTP_REQUEST_METHOD_NOT_SUPPORTED(METHOD_NOT_ALLOWED, "지원하지 않는 요청 Method 입니다."),
-    HTTP_MEDIA_TYPE_NOT_SUPPORTED(UNSUPPORTED_MEDIA_TYPE, "지원하지 않는 요청입니다. Content-Type을 확인해주세요."),
+    METHOD_ARGUMENT_NOT_VALID(BAD_REQUEST, "잘못된 요청입니다. 요청 값을 확인해 주세요."),
+    HTTP_MESSAGE_NOT_READABLE(BAD_REQUEST, "요청 필드의 값이 잘못되었습니다."),
+    HTTP_MEDIA_TYPE_NOT_ACCEPTABLE(BAD_REQUEST, "응답 형식을 처리할 수 없습니다. 잠시 후 다시 시도해 주세요."),
+    HTTP_REQUEST_METHOD_NOT_SUPPORTED(METHOD_NOT_ALLOWED, "지원하지 않는 요청 Method입니다."),
+    HTTP_MEDIA_TYPE_NOT_SUPPORTED(UNSUPPORTED_MEDIA_TYPE, "지원하지 않는 요청입니다. Content-Type을 확인해 주세요."),
     UNEXPECTED_ERROR(INTERNAL_SERVER_ERROR, "알 수 없는 오류가 발생하였습니다."),
     ;
 
