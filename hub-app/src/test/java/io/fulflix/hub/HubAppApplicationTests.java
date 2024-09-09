8e126fd45c84b3a34e70ec20c6445c463ac9f5de
suin <suin505@gmail.com>
2024-09-09 12:39:09 +0900

    init commit

---
+++ hub-app/src/test/java/io/fulflix/hub/HubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fulflix.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
