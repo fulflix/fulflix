ad3dfd1b7329a2dc3975598998937c99f19677b0
choi-ys <project.log.062@gmail.com>
2024-09-11 00:43:22 +0900

    build: api 단일 진입점 제공을 위한 gateway module 생성
    settings.gradle gateway 모듈 등록
    
    local 환경에서 구동을 위해 필요한 설정 구성
    - logging, eureka

---
+++ gateway/src/main/java/io/fulflix/gateway/GatewayApp.java
@@ -0,0 +1,13 @@
+package io.fulflix.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+
+}
