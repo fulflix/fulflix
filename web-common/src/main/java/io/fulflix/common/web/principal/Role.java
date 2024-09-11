541ef9cccd6f3d304173b213439dd0abc97c5049
choi-ys <project.log.062@gmail.com>
2024-09-11 19:27:07 +0900

    feat: 사용자의 권한 정보 객체 공통 모듈로 이관

---
+++ web-common/src/main/java/io/fulflix/common/web/principal/Role.java
@@ -0,0 +1,28 @@
+package io.fulflix.common.web.principal;
+
+import java.util.Arrays;
+import lombok.Getter;
+
+@Getter
+public enum Role {
+    MASTER_ADMIN("시스템 관리자"),
+    HUB_ADMIN("허브 관리자"),
+    HUB_COMPANY("허브 업체"),
+    SUPPLY_COMPANY("공급 업체"),
+    HUB_DELIVERY_MANAGER("허브 배송 담당자"),
+    COMPANY_DELIVERY_MANAGER("업체 배송 담당자"),
+    ANONYMOUS("익명 사용자");
+
+    private final String description;
+
+    Role(String description) {
+        this.description = description;
+    }
+
+    public static Role valueOfRoleName(String roleName) {
+        return Arrays.stream(values())
+            .filter(it -> it.name().equals(roleName))
+            .findFirst()
+            .orElse(ANONYMOUS);
+    }
+}
