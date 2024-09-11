e6199ac5e9c40b198f56c8a6d2d640bef1c34e69
choi-ys <project.log.062@gmail.com>
2024-09-11 17:51:03 +0900

    feat: 내 정보 조회 API 구현
    - @CurrentUser를 이용한 현재 사용자 정보를 통한 내 정보 조회 API 구현

---
+++ user-app/src/main/java/io/fulflix/user/application/UserMyPageService.java
@@ -0,0 +1,23 @@
+package io.fulflix.user.application;
+
+import io.fulflix.user.api.mypage.dto.UserResponse;
+import io.fulflix.user.exception.UserErrorCode;
+import io.fulflix.user.exception.UserException;
+import io.fulflix.user.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserMyPageService {
+
+    private final UserRepo userRepo;
+
+    public UserResponse loadUserById(Long id) {
+        return userRepo.findById(id).map(UserResponse::from)
+            .orElseThrow(() -> new UserException(UserErrorCode.NOT_EXIST, id));
+    }
+
+}
