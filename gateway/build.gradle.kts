3b069d1a383f9186e515733e8bfc47f622449711
choi-ys <project.log.062@gmail.com>
2024-09-11 16:53:21 +0900

    config: jwt 검증을 위한 의존성 추가 및 환경 설정 구성

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -1,3 +1,11 @@
+object DependencyVersions {
+    const val JJWT_VERSION = "0.12.6"
+}
+
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("io.jsonwebtoken:jjwt-api:${DependencyVersions.JJWT_VERSION}")
+    implementation("io.jsonwebtoken:jjwt-jackson:${DependencyVersions.JJWT_VERSION}")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:${DependencyVersions.JJWT_VERSION}")
+
 }
