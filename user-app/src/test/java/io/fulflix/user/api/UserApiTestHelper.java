af68a91dcca7eb385c1312ee99953d3e1117181f
choi-ys <project.log.062@gmail.com>
2024-09-12 23:32:46 +0900

    test: 내 정보 조회 API Controller TC 작성

--- user-app/src/test/java/io/fulflix/user/api/UserApiTestHelper.java
+++ user-app/src/test/java/io/fulflix/user/api/UserApiTestHelper.java
@@ -2,14 +2,22 @@ package io.fulflix.user.api;
 
 import io.fulflix.common.web.base.presentation.WebMvcTestBase;
 import io.fulflix.user.api.authenticate.UserAuthenticateController;
+import io.fulflix.user.api.mypage.UserMyPageController;
 import io.fulflix.user.application.UserAuthenticateUseCase;
+import io.fulflix.user.application.UserMyPageService;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-@WebMvcTest(controllers = UserAuthenticateController.class)
+@WebMvcTest(controllers = {
+    UserAuthenticateController.class,
+    UserMyPageController.class
+})
 public class UserApiTestHelper extends WebMvcTestBase {
 
     @MockBean
     protected UserAuthenticateUseCase userAuthenticateUseCase;
+
+    @MockBean
+    protected UserMyPageService userMyPageService;
 
 }
