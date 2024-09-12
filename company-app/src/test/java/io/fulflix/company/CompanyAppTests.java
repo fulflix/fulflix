6b60f1768cd9ecbe622f8a0dd2db585a6cae3f2e
yezy <iyeahji@gmail.com>
2024-09-12 17:47:32 +0900

    build: company-app 모듈 세팅
    - entry-point 수정
      - AS-IS : CompanyAppApplication
      - TO-BE : CompanyApp
    - entry-point 설정 추가
    - company-app build.gradle에 app-common 모듈 의존성 추가
    - company-app 실행 확인 TC 추가

--- company-app/src/test/java/io/fulflix/company/CompanyAppTests.java
+++ company-app/src/test/java/io/fulflix/company/CompanyAppTests.java
@@ -2,9 +2,11 @@ package io.fulflix.company;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
-class CompanyAppApplicationTests {
+class CompanyAppTests {
 
     @Test
     void contextLoads() {
