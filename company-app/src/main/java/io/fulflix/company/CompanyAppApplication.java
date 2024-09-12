f9fcf556dd85e39c6ed0dc3515424744b23d9bca
yezy <iyeahji@gmail.com>
2024-09-12 16:45:21 +0900

    init commit

---
+++ company-app/src/main/java/io/fulflix/company/CompanyAppApplication.java
@@ -0,0 +1,13 @@
+package io.fulflix.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyAppApplication.class, args);
+    }
+
+}
---
+++ company-app/src/test/java/io/fulflix/company/CompanyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fulflix.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
