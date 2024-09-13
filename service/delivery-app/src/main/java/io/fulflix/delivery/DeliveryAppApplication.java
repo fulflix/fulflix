c591b257a884447d638e6946f077dc7e9898f1ad
suin <suin505@gmail.com>
2024-09-13 19:02:52 +0900

    build: delivery-app 모듈 세팅

---
+++ service/delivery-app/src/main/java/io/fulflix/delivery/DeliveryAppApplication.java
@@ -0,0 +1,26 @@
+package io.fulflix.delivery;
+
+import io.fulflix.common.web.config.YamlPropertySourceFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+import static io.fulflix.common.web.utils.PropertiesCombineUtils.BASE_PACKAGE;
+
+@PropertySources({
+    @PropertySource(
+            name = "delivery-app-properties",
+            value = "classpath:application-delivery.yml",
+            factory = YamlPropertySourceFactory.class
+    )
+})
+//@EnableFeignClients(basePackages = FEIGN_CLIENT_BASE_PACKAGE)
+@SpringBootApplication(scanBasePackages = BASE_PACKAGE)
+public class DeliveryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryAppApplication.class, args);
+    }
+
+}
