481197788dc8c654414e667b401b227d25b4091c
choi-ys <project.log.062@gmail.com>
2024-09-18 00:40:57 +0900

    build: app-core 모듈에 위치한 Auditable 객체의 q-class 생성을 위한 의존성 설정 추가

--- core/app-core/build.gradle.kts
+++ core/app-core/build.gradle.kts
@@ -16,6 +16,7 @@ artifacts {
     add("archives", testJar)
 }
 
+val queryDslVersion = dependencyManagement.importedProperties["querydsl.version"]
 val webCore = ":web-core"
 
 dependencies {
@@ -23,7 +24,26 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     api("org.springframework.cloud:spring-cloud-starter-openfeign")
 
+    implementation("com.querydsl:querydsl-jpa:$queryDslVersion:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+
     runtimeOnly("com.mysql:mysql-connector-j")
 
     testImplementation(project(path = webCore, configuration = "archives"))
+}
+
+// query-dsl auditable q-class generate and delete configurations
+tasks.withType<JavaCompile>().configureEach {
+    options.generatedSourceOutputDirectory.set(file("src/main/generated"))
+    options.compilerArgs.addAll(
+        listOf(
+            "-Aquerydsl.generatedAnnotationClass=javax.annotation.Generated"
+        )
+    )
+}
+
+tasks.named<Delete>("clean") {
+    delete(file("src/main/generated"))
 }
