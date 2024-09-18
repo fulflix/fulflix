0791b41cb18f30a8bbddb45742b24e30ecd572c8
choi-ys <project.log.062@gmail.com>
2024-09-19 04:05:52 +0900

    enhance: feign-client ErrorDecoder 수정
    - AS-IS : 응답 코드가 200이 아닌 경우 로깅 후 RuntimeException Throws
    - TO-BE : 응답 코드가 200이 아닌 경우 로깅 및 Exception 파싱 후 BusinessException Throws

--- core/app-core/src/main/java/io/fulflix/core/app/feign/FeignClientErrorDecoderConfig.java
+++ core/app-core/src/main/java/io/fulflix/core/app/feign/FeignClientErrorDecoderConfig.java
@@ -2,28 +2,59 @@ package io.fulflix.core.app.feign;
 
 import static org.apache.commons.lang.StringUtils.EMPTY;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Request;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import io.fulflix.core.web.exception.BusinessException;
+import io.fulflix.core.web.exception.response.GlobalErrorResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Objects;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StreamUtils;
 
 @Slf4j
+@RequiredArgsConstructor
 public class FeignClientErrorDecoderConfig {
+
+    private final ObjectMapper objectMapper;
 
     @Bean
     public ErrorDecoder decoder() {
 
         return (methodKey, response) -> {
-            Request request = response.request();
-            log.error(""" 
+            GlobalErrorResponse errorResponse = extractError(response);
+            loggingError(methodKey, response, errorResponse);
+
+            throw new BusinessException(
+                HttpStatus.valueOf(response.status()),
+                errorResponse.getCode(),
+                errorResponse.getMessage()
+            );
+        };
+    }
+
+    private GlobalErrorResponse extractError(Response response) {
+        try (InputStream responseBodyStream = response.body().asInputStream()) {
+            String body = StreamUtils.copyToString(responseBodyStream, StandardCharsets.UTF_8);
+            return objectMapper.readValue(body, GlobalErrorResponse.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void loggingError(String methodKey, Response response, GlobalErrorResponse errorResponse) {
+        Request request = response.request();
+
+        log.error(""" 
                     \s
                         Request Fail: {}
                         URL: {} {}
@@ -32,17 +63,14 @@ public class FeignClientErrorDecoderConfig {
                         Request Body: {}
                         Response Body: {}
                     \s""",
-                methodKey,
-                request.httpMethod(),
-                request.url(),
-                response.status(),
-                extractRequestHeader(request),
-                extractRequestBody(request),
-                extractResponseBody(response)
-            );
-
-            return new RuntimeException();
-        };
+            methodKey,
+            request.httpMethod(),
+            request.url(),
+            response.status(),
+            extractRequestHeader(request),
+            extractRequestBody(request),
+            errorResponse
+        );
     }
 
     private static String extractRequestHeader(Request request) {
@@ -61,14 +89,6 @@ public class FeignClientErrorDecoderConfig {
             new String(body, StandardCharsets.UTF_8);
         }
         return EMPTY;
-    }
-
-    private String extractResponseBody(Response response) {
-        try (InputStream responseBodyStream = response.body().asInputStream()) {
-            return IOUtils.toString(responseBodyStream, String.valueOf(StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
 }
