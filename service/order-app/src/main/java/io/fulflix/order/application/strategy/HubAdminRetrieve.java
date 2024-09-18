f2266a9a0c0e4ee656f22e03a1dcabe5274e7e50
yezy <iyeahji@gmail.com>
2024-09-18 17:25:35 +0900

    feat: 주문 전체 조회 및 검색 (허브 관리자)
    - 모든 업체의 주문 전체 조회 및 검색
    - 주문 수량을 입력하면 그 수량 이상인 것들만 전체 조회
    - 취소된 주문까지 전체 조회

---
+++ service/order-app/src/main/java/io/fulflix/order/application/strategy/HubAdminRetrieve.java
@@ -0,0 +1,36 @@
+package io.fulflix.order.application.strategy;
+
+import io.fulflix.common.web.principal.Role;
+import io.fulflix.order.api.dto.OrderDetailResponse;
+import io.fulflix.order.application.OrderRetrieveStrategy;
+import io.fulflix.order.repo.OrderRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class HubAdminRetrieve implements OrderRetrieveStrategy {
+    private final OrderRepo orderRepo;
+
+    @Override
+    public Page<OrderDetailResponse> retrieveOrders(Integer orderQuantity, Pageable pageable, Long currentUser, Role role) {
+        if (hasQuery(orderQuantity)) {
+            return orderRepo.findByOrderQuantityGreaterThanEqual(orderQuantity, pageable)
+                    .map(OrderDetailResponse::fromEntity);
+        }
+
+        return orderRepo.findAll(pageable).map(OrderDetailResponse::fromEntity);
+    }
+
+    @Override
+    public boolean isMatched(Role role) {
+        return role.isHubAdmin();
+    }
+
+    @Override
+    public boolean hasQuery(Integer orderQuantity) {
+        return orderQuantity != null && orderQuantity > 0;
+    }
+}
