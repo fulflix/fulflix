104b0c18d993c389e49b371f4f4f505cb789f320
choi-ys <project.log.062@gmail.com>
2024-09-19 03:53:38 +0900

    fix: gateway ExceptionHandler 수정
    - AS-IS : 동작 하지 않는 @ExceptionHandler 기반 예외 처리
    - TO-BE : Webflux 기반 예외 처리

--- infrastructure/gateway/src/main/java/io/fulflix/gateway/exception/GlobalReactiveExceptionHandler.java
+++ infrastructure/gateway/src/main/java/io/fulflix/gateway/exception/GlobalReactiveExceptionHandler.java
@@ -3,29 +3,49 @@ package io.fulflix.gateway.exception;
 import io.fulflix.gateway.authenticate.exception.UnAuthorizedException;
 import java.net.URI;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Slf4j
-@ControllerAdvice
+@Configuration
 public class GlobalReactiveExceptionHandler {
-    @ExceptionHandler(UnAuthorizedException.class)
-    public Mono<ResponseEntity<String>> handleUnAuthorizedException(UnAuthorizedException exception, ServerWebExchange exchange) {
-        ServerHttpRequest request = exchange.getRequest();
+
+    @Bean
+    @Order(-2)
+    public ErrorWebExceptionHandler globalErrorWebExceptionHandler() {
+        return (exchange, ex) -> {
+            if (ex instanceof UnAuthorizedException unAuthorizedException) {
+                HttpStatus status = unAuthorizedException.getStatus();
+                String message = unAuthorizedException.getMessage();
+
+                exchange.getResponse().setStatusCode(status);
+                exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+                DataBuffer dataBuffer = exchange.getResponse()
+                    .bufferFactory()
+                    .wrap(message.getBytes());
+
+                logging(exchange.getRequest(), status, message);
+
+                return exchange.getResponse().writeWith(Mono.just(dataBuffer));
+            }
+            return Mono.error(ex);
+        };
+    }
+
+    private void logging(ServerHttpRequest request, HttpStatus status, String message) {
         String id = request.getId();
         String method = request.getMethod().name();
         URI uri = request.getURI();
-        HttpStatus status = exception.getStatus();
-        String message = exception.getMessage();
 
         log.error("[{}][{}][{}][{}][{}]", id, method, uri, status, message);
-
-        // 에러 응답 생성
-        return Mono.just(ResponseEntity.status(status).body(message));
     }
+
 }
